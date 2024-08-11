bcf8c280a9e1403bbc444ad6337d5f3888d6b08e
Saja98 <sajjad.98kff@gmail.com>
2024-08-11 07:39:58 +0330

    stage 1 finished

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,6 +2,7 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
+import cv2 as cv
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, ExecuteProcess, RegisterEventHandler
